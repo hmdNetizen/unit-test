e336f7e0c8509daca210465d9fa288da5f6a0721
Hamed Jimoh <58299107+hmdNetizen@users.noreply.github.com>
2021-02-09 00:40:51 -0800

    Implemented test for arrays and objects

--- lib.js
+++ lib.js
@@ -5,3 +5,17 @@ module.exports.absolute = function (number) {
 module.exports.greet = function (name) {
   return "Welcome " + name + "!";
 };
+
+module.exports.getCurrencies = () => {
+  return ["USD", "AUD", "EUR"];
+};
+
+module.exports.getProductDetails = function (productId) {
+  return { id: productId, price: 10 };
+};
+
+module.exports.registerUser = function (username) {
+  if (!username) throw new Error("Username is required");
+
+  return { id: new Date().getTime(), username: username };
+};
--- test/lib.test.js
+++ test/lib.test.js
@@ -1,25 +1,56 @@
 const lib = require("../lib");
 
-// describe("absolute", () => {
-//   it("should return a positive number if input is positive", () => {
-//     const result = lib.absolute(1);
-//     expect(result).toBe(1);
-//   });
+describe("absolute", () => {
+  it("should return a positive number if input is positive", () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+  });
 
-//   it("should return a positive number is the input is negative", () => {
-//     const result = lib.absolute(-1);
-//     expect(result).toBe(1);
-//   });
+  it("should return a positive number is the input is negative", () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
 
-//   it("should return 0 if the input is zero", () => {
-//     const result = lib.absolute(0);
-//     expect(result).toBe(0);
-//   });
-// });
+  it("should return 0 if the input is zero", () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
+});
 
 describe("greet", () => {
   it("Should return Welcome Hamed", () => {
     const result = lib.greet("Hamed");
     expect(result).toMatch(/Hamed/);
+  });
+});
+
+describe("getCurrencies", () => {
+  it("Should return an array of currencies", () => {
+    const result = lib.getCurrencies();
+    expect(result).toEqual(expect.arrayContaining(["AUD", "USD", "EUR"]));
+  });
+});
+
+describe("getProductDetails", () => {
+  it("should return the product with the specified id and price", () => {
+    const result = lib.getProductDetails(1);
+
+    expect(result).toMatchObject({ id: 1, price: 10 });
+
+    expect(result).toHaveProperty("id", 1);
+  });
+});
+
+describe("registerUser", () => {
+  it("should throw an error is username is false", () => {
+    expect(() => {
+      lib.registerUser(null).toThrow();
+    });
+  });
+
+  it("should return an object if the username is true", () => {
+    const result = lib.registerUser("Hamed");
+    expect(result).toMatchObject({ username: "Hamed" });
+    expect(result.id).toBeGreaterThan(0);
   });
 });
